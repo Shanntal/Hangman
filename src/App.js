03e7aaadb941ccd461a67c40d34b81129605bf39
Shanntal Morel <51673250+Shanntal@users.noreply.github.com>
2023-01-05 21:21:45 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import React, {Component} from 'react';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+
+
+    render() {
+        console.log('hello')
+        return (
+            <h1>Shanntal</h1>
+        )
+    }
+}
+
+export default App;
+
